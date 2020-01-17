df3b0cbeadd50722a537a9bf314e71a3f27a362b
Mai <mailen.havela@southworks.com>
2020-01-17 10:48:07 -0300

    WIP: Implement getters on  SlackMessage class

--- slack_adapter/slack_message.py
+++ slack_adapter/slack_message.py
@@ -1,29 +1,28 @@
-class SlackMessage():
+class SlackMessage:
     @property
-    def type(self):
-        pass
+    def type(self) -> str:
+        return self._type
 
     @property
-    def subtype(self):
-        pass
+    def subtype(self) -> str:
+        return self._subtype
 
     @property
-    def text(self):
-        pass
+    def text(self) -> str:
+        return self._text
 
     @property
-    def time_stamp(self):
-        pass
+    def time_stamp(self) -> str:
+        return self._time_stamp
 
     @property
-    def usarname(self):
-        pass
+    def usarname(self) -> str:
+        return self._usarname
 
     @property
-    def bot_id(self):
-        pass
+    def bot_id(self) -> str:
+        return self._bot_id
 
     @property
-    def thread_time_stamp(self):
-        pass
-
+    def thread_time_stamp(self) -> str:
+        return self._thread_time_stamp
