25cc81066e97a5b8cfb03c1cbab2b849d0cb0005
Sergio <44620563+SergioHornik@users.noreply.github.com>
2020-01-20 12:38:19 -0300

    port slack payload

--- slack_adapter/slack_payload.py
+++ slack_adapter/slack_payload.py
@@ -1,32 +1,75 @@
 class SlackPayload:
     @property
     def type(self):
-        pass
+        return self._type
+
+    @type.setter
+    def type(self, type):
+        self._type = type
 
     @property
     def token(self):
-        pass
+        return self._token
+
+    @token.setter
+    def token(self, token):
+        self._token = token
 
     @property
     def channel(self):
-        pass
+        return self._channel
+
+    @channel.setter
+    def channel(self, channel):
+        self._channel = channel
 
     @property
     def thread_time_stamp(self):
-        pass
+        return self._thread_time_stamp
+
+    @thread_time_stamp.setter
+    def thread_time_stamp(self, thread_time_stamp):
+        self._thread_time_stamp = thread_time_stamp
 
     @property
     def team(self):
-        pass
+        return self._team
+
+    @team.setter
+    def team(self, team):
+        self._team = team
 
     @property
     def message(self):
-        pass
+        return self._message
+
+    @message.setter
+    def message(self, message):
+        self._message = message
 
     @property
     def user(self):
-        pass
+        return self._user
+
+    @user.setter
+    def user(self, user):
+        self._user = user
 
     @property
     def actions(self):
-        pass
+        return self._actions
+
+    @actions.setter
+    def actions(self, actions):
+        self._actions = actions
+
+    def __init__(self, type=None, token=None, channel=None, thread_time_stamp= None, team=None,
+                 message=None, user=None, actions=None):
+        self._type = type
+        self._token = token
+        self._channel = channel
+        self._thread_time_stamp = thread_time_stamp
+        self._team = team
+        self._message = message
+        self._user = user
+        self._actions = actions
