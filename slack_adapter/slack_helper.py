20ab41680cce6828b8c594a35f74d537adda4c00
Konigstiger <mariano.rodriguez@southworks.com>
2020-01-22 17:39:15 -0300

    Update slack_helper.py
    Changes:
    - Add references to slack.web.classes (for using SlackAPI)
    - Refactor and clean method activity_to_slack
    - Removed TODO's

--- slack_adapter/slack_helper.py
+++ slack_adapter/slack_helper.py
@@ -2,10 +2,13 @@ from datetime import datetime
 from typing import List, Dict
 
 import slack
-from botbuilder.schema import Activity, ConversationAccount, ChannelAccount, ActivityTypes
+from botbuilder.schema import Activity, ConversationAccount, ChannelAccount, ActivityTypes, Attachment
 
 from slack_adapter import NewSlackMessage, SlackRequestBody, SlackPayload
 from slack_adapter.slack_client_wrapper import SlackClientWrapper
+
+from slack.web.classes import attachments
+
 
 Object = lambda **kwargs: type("Object", (), kwargs)
 
@@ -14,75 +17,36 @@ class SlackHelper:
 
     @staticmethod
     def activity_to_slack(activity: Activity) -> NewSlackMessage:
-        """ Formats a BotBuilder activity into an outgoing Slack message.
-        :param activity: A BotBuilder Activity object.
-        :return: A Slack message object with {text, attachments, channel, thread ts}
-            as well as any fields found in activity.channelData.
-        """
-        attachments = []
+        # attachments = List[slack.web.classes.attachments.Attachment]
 
         if not activity:
             raise Exception(activity.name)
 
         message = NewSlackMessage()
+
         if not activity.timestamp:
-            # TODO: Property time_stamp cannot be set
             message.time_stamp = activity.timestamp
 
-        # TODO: Property text cannot be set
         message.text = activity.text
 
         if activity.attachments:
-            # TODO: Define a class that is similar to SlackAPI.Attachment (.net)
-            """ 
-            namespace SlackAPI
-            {
-                public class Attachment
-                {
-                    public string callback_id;
-                    public string footer;
-                    public AttachmentAction[] actions;
-                    public string[] mrkdwn_in;
-                    public string thumb_url;
-                    public string image_url;
-                    public IBlock[] blocks;
-                    public Field[] fields;
-                    public string footer_icon;
-                    public string text;
-                    public string title;
-                    public string author_icon;
-                    public string author_link;
-                    public string author_name;
-                    public string pretext;
-                    public string color;
-                    public string fallback;
-                    public string title_link;
-
-                    public Attachment();
-                }
-            }
-            """
-            # attachments: List[type] = list()
-            attachments = list()
+            attachment_list: List[slack.web.classes.attachments.Attachment] = list()
 
             for att in activity.attachments:
                 if att.name == 'blocks':
                     message.blocks = [att.content]
                 else:
-                    # TODO: Object definition, what class should it be? SlackAPI.Attachment
-                    new_attachment = Object(author_name=att.name, thumb_url=att.thumbnail_url)
-                    attachments.append(new_attachment)
+                    new_attachment: slack.web.classes.attachments.Attachment
+                    new_attachment = slack.web.classes.attachments.Attachment(author_name=att.name, thumb_url=att.thumbnail_url)
+                    attachment_list.append(new_attachment)
 
-            if len(attachments) > 0:
-                # TODO: Property attachments cannot be set
-                message.attachments = attachments
+            if len(attachment_list) > 0:
+                message.attachments = attachment_list
 
-        # TODO: Property channel cannot be set
         # TODO: Verify that the id field exists
         message.channel = activity.conversation.id
 
         if activity.conversation["thread_ts"]:
-            # TODO: Property thread_time_stamp cannot be set
             message.thread_time_stamp = str(activity.conversation["thread_ts"])
 
         # if channelData is specified, overwrite any fields in message object
